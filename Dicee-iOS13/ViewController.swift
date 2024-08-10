e6afe28b694a102bacbc4224feb67e627554a062
minh <luongthanhminh369@gmail.com>
2024-08-10 13:25:00 -0700

    competed dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        //WHO           WHAT = VALUE
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix");
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceOne");
+//        diceImageView1.alpha = 0.5;
+//        diceImageView2.alpha = 0.5;
+//    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")];
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+//        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
-
-
 }
-
+        
